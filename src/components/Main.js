d91a0afa89b0de514def7fd60f850fc346c10e19
Heather Holcomb <holcombheather@gmail.com>
2023-04-17 21:36:55 -0700

    updated and validated map image rendering

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import { CardImg } from 'react-bootstrap';
 
 class Main extends Component {
     constructor(props) {
@@ -10,7 +12,8 @@ class Main extends Component {
         this.state = {
             city: '',
             cityData: [],
-            error: false
+            error: false,
+            errorMsg: ''
         }
     }
 
@@ -21,48 +24,45 @@ class Main extends Component {
         })
     }
 
-   
-        
-
-    // TODO: constructor method to hand data from form
-    // TODO: handle error 
-
-    // Method that queries LocationIQ for long/lat 
+    // Method that queries LocationIQ for long/lat
     getCityData = async (ev) => {
         ev.preventDefault();
+
         try {
-        let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&q=${this.state.city}&format=json`
-        
-        let cityData = await axios.get(url)
+            // Define URL to pass to axios using the city in state
+            let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&q=${this.state.city}&format=json`
+            
+            // Call to the location IQ API using axios
+            let response = await axios.get(url)
+            let cityData = response.data[0];
 
-        console.log(cityData.data[0])
+            console.log(cityData);
 
-        this.setState({
-            cityData: cityData.data[0],
-            error:false
-        })
-    }   catch(error){
+            let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&center=${cityData.lat},${cityData.lon}&zoom=13`
+
+
+            // Take return from axios and set that to state
+            this.setState({
+                cityData: cityData,
+                mapUrl: mapUrl,
+                error: false
+            })
+
+        // Set state with the error boolean and error message
+        } catch(error){
         this.setState({
             error: true,
             errorMsg: error.message
         })
-    }
+        }
 
     }
-
-    // TODO: constructor method to query LocationIQ for long/lat
-    // TODO: Define URL to pass to axios using the city in state
-
-    // TODO: Call to the location IQ API using axios
-
-    // TODO: Take return from axios and set that to state
-
-    // TODO: Set state with the error boolean and error message
 
     render() {
         return (
             <>
-                <Form onSubmit={this.getCityData}>
+            <div>
+                                <Form onSubmit={this.getCityData}>
                     <Form.Group>
                         <Form.Label>Location</Form.Label>
                         <Form.Control type="text" onInput={this.handleCityInput} />
@@ -70,18 +70,23 @@ class Main extends Component {
                     </Form.Group>
                     <Button variant="primary" type="submit">Explore!</Button>
                 </Form>
-                    {/* //TO DO: Component to render results of display_name, latitude, longitude
-                    //TO DO: Display error message */}
-                <Card>
-                    <Card.Text>
-                        <ul>
-                            <li>City: {this.state.cityData.display_name}</li>
-                            <li>Longitude: {this.state.cityData.lon}</li>
-                            <li>Latitude: {this.state.cityData.lat}</li>
-                        </ul>
-                        
-                    </Card.Text>
-                </Card>
+            </div>
+            <div>
+                        <Card>
+                    <Card.Body>
+                        {
+                        this.state.error 
+                        ? <Card.Text>{this.state.errorMsg}</Card.Text>
+                        : <div>
+                            <p>City: {this.state.cityData.display_name}</p>
+                            <p>Longitude: {this.state.cityData.lon}</p>
+                            <p>Latitude: {this.state.cityData.lat}</p>
+                            <CardImg variant="bottom" src={this.state.mapUrl}></CardImg>
+                        </div>}
+                    </Card.Body>
+                </Card>        
+            </div>
+
             </>
         )
 
