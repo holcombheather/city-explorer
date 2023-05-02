ad92cad07201c5c37c2566ae7bf08f70364822f2
Heather Holcomb <holcombheather@gmail.com>
2023-05-01 22:54:18 -0700

    updated componentizing

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,47 +1,75 @@
-import { Component } from "react";
-import { Col, Card, CardGroup } from 'react-bootstrap';
+import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+import WeatherDay from './WeatherDay';
 
-class Weather extends Component {
-    render(){
-        console.log('Weather props: ', this.props.showWeather);
-        
-        let forecastCards;
-        if (this.props.forecasts) {
-            forecastCards = this.props.forecasts.map((forecast) => {
-                return <Col xs={6} md={3}>
-                    <Card>
-                        <Card.Header>{forecast.date}</Card.Header>
-                        <Card.Body>
-                            <Card.Text>{forecast.description}</Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            });
-        }
+class Weather extends React.Component {
+  render() {
+    const weatherData = this.props.showWeather;
 
-        return (
-            <>            
-            <Card >            
-                <Card.Header as="h5" style={{marginTop: '10px'}}>Weather Forecast:</Card.Header>
-                <Card.Body>
-                    <CardGroup>
-                       {forecastCards}
-                    </CardGroup>
-                </Card.Body>
-            </Card>
-            </>
-        )
+    let forecastCards;
+    if (weatherData) {
+      forecastCards = weatherData.map((forecast) => (
+        <WeatherDay
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+        />
+      ));
     }
+
+    return (
+      <>
+        <Card>
+          <Card.Header as="h5" style={{marginTop: '10px'}}>Weather Forecast:</Card.Header>
+          <Card.Body>
+            <CardGroup>
+              {forecastCards}
+            </CardGroup>
+          </Card.Body>
+        </Card>
+      </>
+    );
+  }
 }
 
 export default Weather;
-   
-   
-   // handleWeather = async (lat, lon) => {
-    //     let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${lat}&lon=${lon}&searchQuery=${this.state.city}`;
-        
-    //     let weatherData = await axios.get(weatherUrl);
-    // }
 
-                // this.handleWeather(cityData.data[0].lat, cityData.data[0].lon);
-            // let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}&searchQuery=${this.state.city}`;
+
+// import { Component } from "react";
+// import { Col, Card, CardGroup } from 'react-bootstrap';
+
+// class Weather extends Component {
+//     render(){
+//         console.log('Weather props: ', this.props.showWeather);
+        
+//         let forecastCards;
+//         if (this.props.forecasts) {
+//             forecastCards = this.props.forecasts.map((forecast) => {
+//                 return <Col xs={6} md={3}>
+//                     <Card>
+//                         <Card.Header>{forecast.date}</Card.Header>
+//                         <Card.Body>
+//                             <Card.Text>{forecast.description}</Card.Text>
+//                         </Card.Body>
+//                     </Card>
+//                 </Col>
+//             });
+//         }
+
+//         return (
+//             <>            
+//             <Card >            
+//                 <Card.Header as="h5" style={{marginTop: '10px'}}>Weather Forecast:</Card.Header>
+//                 <Card.Body>
+//                     <CardGroup>
+//                        {forecastCards}
+//                     </CardGroup>
+//                 </Card.Body>
+//             </Card>
+//             </>
+//         )
+//     }
+// }
+
+// export default Weather;
+  
